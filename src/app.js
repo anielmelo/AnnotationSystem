30f7d70306da7b8f34589980c3b5097789bfbb92
anielmelo <anielcontato@gmail.com>
2024-03-31 13:17:09 -0300

    update app

--- src/app.js
+++ src/app.js
@@ -1,15 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const router = require('./router')
 
 const app = express()
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS", "PATCH");
-    res.header("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization");
-    next();
-})
-
+app.use(cors())
 app.use(express.json())
 app.use(router)
 

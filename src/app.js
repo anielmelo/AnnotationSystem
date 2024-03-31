c31cc06f7c3dddd3a231b841c07c50e1be01091e
anielmelo <anielcontato@gmail.com>
2024-03-31 11:09:09 -0300

    update app

--- src/app.js
+++ src/app.js
@@ -5,8 +5,8 @@ const app = express()
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS", "PATCH");
+    res.header("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization");
     next();
 })
 
